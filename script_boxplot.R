5bfbe92436ea37b4b498b6dc96bc60dbfe9215b0
Christin Zasada <christin@rladies.org>
2021-04-14 12:31:56 +0200

    Update and rename script2.R to script_boxplot.R
    One nice example of boxplot

---
+++ script_boxplot.R
@@ -0,0 +1,19 @@
+
+#I love boxplots - an example from the R graph gallery
+
+# library
+library(ggplot2)
+library(dplyr)
+library(hrbrthemes)
+
+# Start with the diamonds dataset, natively available in R:
+p <- diamonds %>%
+  
+  # Add a new column called 'bin': cut the initial 'carat' in bins
+  mutate( bin=cut_width(carat, width=0.5, boundary=0) ) %>%
+  
+  # plot
+  ggplot( aes(x=bin, y=price) ) +
+    geom_boxplot(fill="#69b3a2") +
+    theme_ipsum() +
+    xlab("Carat")
